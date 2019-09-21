d76da8d8ff9e66bc2afddc41729e231afb99ccad
Kostya Zhamoydin <zhamoydin@mail.ru>
2019-09-21 20:53:33 +0300

    add knpPaginator

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,30 +2,44 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomePageController extends AbstractController
+
+class HomePageController extends Controller
 {
     /**
      * @Route("/", name="home_page")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $em = $this->get('doctrine.orm.entity_manager');
+        $dql = "SELECT a FROM AcmeMainBundle:Article a";
+        $query = $em->createQuery($dql);// переменная должна содержать массив массивов с постами
         return $this->render('home_page/index.html.twig', [
             'controller_name' => 'HomePageController',
-            'title' => 'All posts'
+            'title' => 'All posts',
+            'posts' => $this->get('knp_paginator')->paginate(
+                $query, /* query NOT result */
+                $request->query->getInt('page', 1), 10)
         ]);
     }
 
     /**
      * @Route("/popular")
      */
-    public function popular()
+    public function popular(Request $request)
     {
+        $em = $this->get('doctrine.orm.entity_manager');
+        $dql = "SELECT a FROM AcmeMainBundle:Article a";
+        $query = $em->createQuery($dql);// переменная должна содержать массив массивов с постами
         return $this->render('home_page/index.html.twig', [
             'controller_name' => 'HomePageController',
-            'title' => 'Popular'
+            'title' => 'Popular',
+            'posts' => $this->get('knp_paginator')->paginate(
+                $query, /* query NOT result */
+                $request->query->getInt('page', 1), 10)
         ]);
     }
 }
