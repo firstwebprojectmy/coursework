c1720eff74739ea8aa34dafe62fc197f42df78a4
Kostya Zhamoydin <zhamoydin@mail.ru>
2019-09-22 22:47:19 +0300

    fix blogger page

--- src/Migrations/Version20190922190243.php
+++ src/Migrations/Version20190922190243.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190922122354 extends AbstractMigration
+final class Version20190922190243 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,9 +22,12 @@ final class Version20190922122354 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE post (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, created_at DATETIME NOT NULL, short_description VARCHAR(255) NOT NULL, text_description LONGTEXT NOT NULL, image_url VARCHAR(255) DEFAULT NULL, INDEX IDX_5A8A6C8DA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `like` (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, post_id INT DEFAULT NULL, INDEX IDX_AC6340B3A76ED395 (user_id), INDEX IDX_AC6340B34B89032C (post_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE post (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, created_at DATETIME NOT NULL, short_description VARCHAR(255) NOT NULL, text_description LONGTEXT NOT NULL, image_url VARCHAR(255) DEFAULT NULL, is_banned TINYINT(1) NOT NULL, INDEX IDX_5A8A6C8DA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE preferences (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, blogger_id INT NOT NULL, INDEX IDX_E931A6F5A76ED395 (user_id), INDEX IDX_E931A6F5D700BD1D (blogger_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, second_name VARCHAR(255) NOT NULL, is_banned TINYINT(1) NOT NULL, is_confirmed TINYINT(1) NOT NULL, confirme_hash VARCHAR(32) NOT NULL, regisreted_at DATE NOT NULL, image_url VARCHAR(255) DEFAULT NULL, is_blogger TINYINT(1) NOT NULL, is_comfirmed_by_moderator TINYINT(1) DEFAULT NULL, short_information LONGTEXT DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE `like` ADD CONSTRAINT FK_AC6340B3A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE `like` ADD CONSTRAINT FK_AC6340B34B89032C FOREIGN KEY (post_id) REFERENCES post (id)');
         $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8DA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE preferences ADD CONSTRAINT FK_E931A6F5A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE preferences ADD CONSTRAINT FK_E931A6F5D700BD1D FOREIGN KEY (blogger_id) REFERENCES user (id)');
@@ -35,9 +38,12 @@ final class Version20190922122354 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE `like` DROP FOREIGN KEY FK_AC6340B34B89032C');
+        $this->addSql('ALTER TABLE `like` DROP FOREIGN KEY FK_AC6340B3A76ED395');
         $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8DA76ED395');
         $this->addSql('ALTER TABLE preferences DROP FOREIGN KEY FK_E931A6F5A76ED395');
         $this->addSql('ALTER TABLE preferences DROP FOREIGN KEY FK_E931A6F5D700BD1D');
+        $this->addSql('DROP TABLE `like`');
         $this->addSql('DROP TABLE post');
         $this->addSql('DROP TABLE preferences');
         $this->addSql('DROP TABLE user');
